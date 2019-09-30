869ac94b637c6c0423f9f58a11b6edbc68908197
James Coleman <jcolem22@msudenver.edu>
2019-09-30 17:18:03 -0600

    Added the following validations; presence, uniquness and range (for car year)

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,5 @@
 class Make < ApplicationRecord
   has_many :cars
+  validates :name, :country, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,5 @@
 class Part < ApplicationRecord
   has_and_belongs_to_many :cars
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
 end

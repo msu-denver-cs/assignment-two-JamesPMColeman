377d0701f30ca3441e967ed592d67213b4f793fc
James Coleman <jcolem22@msudenver.edu>
2019-10-02 13:08:42 -0600

    Added some seed data

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :make
   has_and_belongs_to_many :parts
-  validates :model, :VIN, :year, presence: true
+  validates :make, :model, :VIN, :year, presence: true
   validates :year, numericality: {
       greater_than_or_equal_to: 1885, less_than_or_equal_to: Date.today.year }
-  validates :model, :VIN, uniqueness: true
+  validates :VIN, uniqueness: true
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -8,4 +8,37 @@ class CarTest < ActiveSupport::TestCase
     assert car.errors[:year].any?
     assert car.errors[:VIN].any?
   end
+  #test "year must be 1885 or later" do
+  #  car = Car.new(model:    "Some car",
+  #                VIN:      123874)
+  #  car.year = -1
+  #  assert car.invalid?
+  #  assert_equal ["must be greater than or equal to 1885"],
+  #               car.errors[:year]
+
+  #  car.year = 1800
+  #  assert car.invalid?
+  #  assert_equal ["must be greater than or equal to 1885"],
+  #               car.errors[:year]
+
+  #  car.year = 1885
+  #  assert car.valid?
+  #end
+  #test "year must be the current year or earlier" do
+  #  car = Car.new(make:     Make.first,
+  #                model:    "Some car",
+  #                VIN:      123874)
+  #  car.year = 10100
+  #  assert car.invalid?
+  #  assert_equal ["must be less than or equal to the current year"],
+  #               car.errors[:year]
+
+  #  car.year = Date.today.year + 1
+  #  assert car.invalid?
+  #  assert_equal ["must be less than or equal to the current year"],
+  #               car.errors[:year]
+
+  #  car.year = 2000
+  #  assert car.valid?
+  #end
 end
